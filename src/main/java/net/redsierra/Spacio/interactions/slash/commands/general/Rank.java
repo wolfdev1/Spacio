9d6b762e327ccd0ae6735ad1ae604fecba9ffcdd
wolfdev1 <wolfdev1@outlook.com>
2023-11-19 18:46:53 +0100

    Update Rank card

--- src/main/java/net/redsierra/Spacio/interactions/slash/commands/general/Rank.java
+++ src/main/java/net/redsierra/Spacio/interactions/slash/commands/general/Rank.java
@@ -5,95 +5,103 @@ import com.mongodb.client.MongoDatabase;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.utils.FileUpload;
-import net.redsierra.Spacio.Spacio;
 import net.redsierra.Spacio.config.BotConfig;
 import net.redsierra.Spacio.events.SlashCommandInteraction;
 import net.redsierra.Spacio.interactions.Command;
 import net.redsierra.Spacio.util.Resources;
 import org.bson.Document;
-import org.slf4j.Logger;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URL;
-import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("ALL")
 public class Rank extends Command {
+    private static final int IMAGE_WIDTH = 736;
+    private static final int IMAGE_HEIGHT = 414;
+    private static final int PFP_WIDTH = 221;
+    private static final int PFP_HEIGHT = 207;
+    private static final int PFP_X = 91;
+    private static final int PFP_Y = 150;
+    private static final int USER_NAME_X = PFP_X - 1;
+    private static final int USER_NAME_Y = PFP_Y - 63;
+    private static final int LEVEL_X = IMAGE_WIDTH / 2 + 44;
+    private static final int LEVEL_Y = 250;
+    private static final int XP_X = IMAGE_WIDTH / 2 + 34;
+    private static final int XP_Y = 310;
 
+    private static final BufferedImage BACKGROUND_IMAGE;
+    private static final Font FONT_PLAIN_34;
+    private static final Font FONT_PLAIN_38;
+
+    static {
+        try {
+            Resources resources = new Resources();
+            BACKGROUND_IMAGE = ImageIO.read(resources.getResourceFile("img/rank_bg.png"));
+
+            Font ftest = Font.createFont(Font.TRUETYPE_FONT, resources.getResourceFile("font/OpenSans-ExtraBold.ttf"));
+            FONT_PLAIN_34 = ftest.deriveFont(Font.PLAIN, 34);
+            FONT_PLAIN_38 = ftest.deriveFont(Font.PLAIN, 38);
+        } catch (IOException | FontFormatException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public Rank() {
         super.setName("rank");
         super.setCategory("info");
         super.setDescription("Return a card with your level and experience if you have a level greater than zero, otherwise it will return an error.");
+
     }
 
     @Override
     public void execute(SlashCommandInteraction ev) {
         SlashCommandInteractionEvent event = ev.getEvent();
-            Spacio bot = new Spacio();
-            Logger logger = bot.logger;
-            MongoDatabase db = null;
-            try {
-                db = new BotConfig().getDatabase();
-                MongoCollection<Document> c = db.getCollection("users");
+        MongoDatabase db = null;
+        try {
+            db = new BotConfig().getDatabase();
+            MongoCollection<Document> c = db.getCollection("users");
             String userId = event.getUser().getId();
 
-            if(c.find(new Document("userId", userId)).first() == null) {
+            Document userDoc = c.find(new Document("userId", userId)).first();
+            if (userDoc == null) {
                 event.reply("Sorry... You have not sent any messages yet! Please try again later").queue();
             } else {
-                Document user = c.find(new Document("userId", userId)).first();
-                assert user != null;
-                int level = user.getInteger("level");
-                int xp = user.getInteger("xp");
+                int level = userDoc.getInteger("level");
+                int xp = userDoc.getInteger("xp");
 
-                try {
-                    InputStream f = this.getImage(event.getUser(), xp, level);
-
-                    event.replyFiles(FileUpload.fromData(f, "rank.png")).queue();
+                try (InputStream f = this.getImage(event.getUser(), xp, level)) {
+                    event.deferReply().queue();
+                    event.getHook().editOriginal("Here is your rank card!").setFiles(FileUpload.fromData(f, "rank.png")).delay(7, TimeUnit.SECONDS).queue();
                 } catch (IOException | FontFormatException e) {
                     throw new RuntimeException(e);
                 }
-
-
             }
-
         } catch (Exception e) {
             event.reply("Sorry... Something went wrong! Please try again later").queue();
-            logger.error("A fatal error occurred while executing the rank command", e);
-          }
+        }
     }
 
     public InputStream getImage(User user, int xp, int level) throws IOException, FontFormatException {
-        Resources resources = new Resources();
-        BufferedImage backgroundImage = ImageIO.read(resources.getResourceFile("img/rank_bg.png"));
-
-        BufferedImage pfp = ImageIO.read(new URL(Objects.requireNonNull(user.getAvatarUrl())));
-
-        BufferedImage image = new BufferedImage(736, 414, BufferedImage.TYPE_INT_RGB);
-
-        Font ftest = Font.createFont(Font.TRUETYPE_FONT, resources.getResourceFile("font/OpenSans-ExtraBold.ttf"));
-        Font font = ftest.deriveFont(Font.PLAIN, 34);
-        Font font1 = ftest.deriveFont(Font.PLAIN, 22);
-        Font font2 = ftest.deriveFont(Font.PLAIN, 38);
+        BufferedImage image = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_INT_ARGB);
 
         Graphics g = image.getGraphics();
 
-        g.drawImage(pfp, 91, 150, 221, 207, null);
-        g.drawImage(backgroundImage, 0, 0, null);
+        g.drawImage(ImageIO.read(new URL(user.getAvatarUrl())), PFP_X, PFP_Y, PFP_WIDTH, PFP_HEIGHT, null);
+        g.drawImage(BACKGROUND_IMAGE, 0, 0, null);
 
         g.setColor(Color.decode("#ffffff"));
-        g.setFont(font);
+        g.setFont(FONT_PLAIN_34);
 
-        g.drawString(user.getGlobalName(), 90, 87);
+        g.drawString(user.getGlobalName(), USER_NAME_X, USER_NAME_Y);
 
-        g.setFont(font2);
+        g.setFont(FONT_PLAIN_38);
         g.setColor(Color.decode("#c9daec"));
         int requiredXP = (level + 1) * 350;
-        g.drawString("Level " + level, 435, 250);
-        g.drawString(xp + "/" + requiredXP, 425, 310);
+        g.drawString("Level " + level, LEVEL_X, LEVEL_Y);
+        g.drawString(xp + "/" + requiredXP, XP_X, XP_Y);
 
         g.dispose();
 
